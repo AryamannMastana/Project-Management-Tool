b8a74f06f0f20dadde0d0615e89741ad51035673
Aryamann <aryamannmastana@gmail.com>
2022-09-03 21:30:30 +0530

    updated login.js

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,35 @@
 // Styles
+import { useState } from 'react'
 import './Login.css'
 
+
 function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <div className="App">
-            Login
-        </div>
-    );
+        <form className="auth-form">
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                required 
+                type='email'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input 
+                required 
+                type='password'
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                />
+            </label>
+            <button className="btn">Login</button>
+        </form>
+        );
 }
 
 export default Login;
